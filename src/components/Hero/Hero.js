ac04640d269ee014440f014e0cd831f3b6a96dcb
islam bouterbiat <islambouter2000@gmail.com>
2021-12-23 21:46:42 +0100

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        Islam Portfolio
+      </SectionTitle>
+      <SectionText>
+        I'm a full-stack developer who specializes in javascript tachnologies
+        across the whole stack (react.js, node.js, express). experience building
+        complete web applications with backend API systems
+      </SectionText>
+      <Button
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/islambouterbiat/", "_blank")
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
