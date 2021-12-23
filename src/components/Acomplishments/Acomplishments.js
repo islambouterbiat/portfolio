992e0439ce8d4b65b0c7884891c31ae00a47e2b8
islam bouterbiat <islambouter2000@gmail.com>
2021-12-23 23:46:21 +0100

    accomplishments section

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 20, text: "Open Source Projects" },
+  { number: 1000, text: "Students" },
+  { number: 1900, text: "Github Followers" },
+  { number: 5000, text: "Github Stars" },
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>Personal Accomplishments</SectionTitle>
+    <Boxes>
+      {data.map((card, i) => (
+        <Box key={i}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
