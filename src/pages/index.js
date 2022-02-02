f03fff56d882114d88102afa6aa378058036ee04
islam bouterbiat <islambouter2000@gmail.com>
2022-02-02 05:20:32 +0100

    fixed some issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation"
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
+import Head from "next/head";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
@@ -10,6 +11,10 @@ import { Section } from "../styles/GlobalComponents";
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>Islam BTR's Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
